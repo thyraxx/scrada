4b82d5e565db5300766a8d42da7838c511625557
X <taz3008@msn.com>
2021-11-25 11:32:28 +0100

    Add retrieving data from database to DTO + added retrieve API data

--- src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
+++ src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
@@ -1,17 +1,17 @@
 package com.thyraxx.scrada.smashgg.service;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.thyraxx.scrada.smashgg.SmashggApi;
-import com.thyraxx.scrada.smashgg.model.EventsModel;
 import com.thyraxx.scrada.smashgg.model.Tournament;
+import com.thyraxx.scrada.smashgg.model.TournamentDTO;
+import com.thyraxx.scrada.smashgg.model.generated.Node;
 import com.thyraxx.scrada.smashgg.repository.SmashggRepository;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SmashggService {
@@ -23,13 +23,28 @@ public class SmashggService {
         this.smashggRepository = smashggRepository;
     }
 
-    public List<Tournament> getAllTournaments()
+    public List<TournamentDTO> getAllTournaments()
     {
-        return smashggRepository.findAll();
+        ModelMapper modelMapper = new ModelMapper();
+        return smashggRepository.findAll().stream()
+                .map(tournament -> modelMapper.map(tournament, TournamentDTO.class))
+                .collect(Collectors.toList());
     }
 
-    public List<Object> getTournamentsEvents(String variables)
+    public List<Tournament> getTournamentsEvents(String variables)
     {
-        return SmashggApi.runAPIQuery(variables);
+        return SmashggApi.getSmashTournamentEvents(variables);
+    }
+
+    public void saveNewTournamentEvents()
+    {
+        // TODO: better var name or separate into it's own config?
+        String epochSecondsParameter = "{ \"lastDate\":" + Instant.now().getEpochSecond() +"}";
+        smashggRepository.saveAll(getTournamentsEvents(epochSecondsParameter));
+    }
+
+    public void save(Tournament tournament)
+    {
+        smashggRepository.save(tournament);
     }
 }
