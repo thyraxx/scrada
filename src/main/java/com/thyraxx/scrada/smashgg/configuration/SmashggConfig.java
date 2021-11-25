9fa5c5f9e815a3904d90e0c006088dd383a417b1
X <taz3008@msn.com>
2021-11-25 11:17:45 +0100

    Added smashgg config

---
+++ src/main/java/com/thyraxx/scrada/smashgg/configuration/SmashggConfig.java
@@ -0,0 +1,9 @@
+package com.thyraxx.scrada.smashgg.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class SmashggConfig {
+
+    // Delay in milliseconds
+    public static final long smashRetrieveDelay = 90000;
+}
