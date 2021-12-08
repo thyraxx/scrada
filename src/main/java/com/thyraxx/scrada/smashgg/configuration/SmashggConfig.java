3e31ca5b6b6c64faf0aec44a5ec76855aaae3ada
X <taz3008@msn.com>
2021-12-08 16:36:44 +0100

    Added search criteria in a config for API

--- src/main/java/com/thyraxx/scrada/smashgg/configuration/SmashggConfig.java
+++ src/main/java/com/thyraxx/scrada/smashgg/configuration/SmashggConfig.java
@@ -1,9 +1,11 @@
 package com.thyraxx.scrada.smashgg.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.time.Instant;
 
 public class SmashggConfig {
 
     // Delay in milliseconds
     public static final long smashggAPICallDelay = 90000;
+    public static final String searchTournamentsAfterEpochTime = "{ \"lastDate\":" + (Instant.now().getEpochSecond() - 262928) +"}"; // 262928 aprox 3 days
+
 }
