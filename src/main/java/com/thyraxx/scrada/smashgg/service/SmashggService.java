cc36c368a958e0c3282c9bafc2c2db99ef0e174a
X <taz3008@msn.com>
2021-12-20 22:56:28 +0100

    Added send message

--- src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
+++ src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
@@ -2,14 +2,18 @@ package com.thyraxx.scrada.smashgg.service;
 
 import com.thyraxx.scrada.smashgg.SmashggApi;
 import com.thyraxx.scrada.smashgg.configuration.SmashggConfig;
+import com.thyraxx.scrada.smashgg.model.Event;
 import com.thyraxx.scrada.smashgg.model.Tournament;
 import com.thyraxx.scrada.smashgg.model.TournamentDTO;
 import com.thyraxx.scrada.smashgg.repository.SmashggRepository;
+import com.thyraxx.scrada.telegrambot.SmashggTelegramBot;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -68,6 +72,29 @@ public class SmashggService {
                         .collect(Collectors.toList());
 
         smashggRepository.deleteAll(tournaments);
+    }
+
+    public void sendNotification()
+    {
+        List<Tournament> tournaments = smashggRepository.findAll();
+
+        tournaments.forEach(tournament -> {
+            if(tournament.isRegistrationOpen() && !tournament.isUserNotifiedAfterOpen())
+            {
+                SmashggTelegramBot.sendMessage(-477115672,
+                        "<b>" + tournament.getName() + "</b> \n\n" +
+                        "<b>Location:</b> " + tournament.getCity() + "\n\n" +
+                        "<b>Date:</b> " + LocalDateTime.ofEpochSecond(tournament.getStartAt(), 0, ZoneOffset.ofHours(1)) + " \n\n" +
+                        "<b>Events:</b> \n\n" +
+                            for(Event event : tournament.getEvents())
+                            {
+                                event.getValueLimit()
+                            }
+                + "https://smash.gg/" + tournament.getSlug()
+
+                );
+            }
+        });
     }
 
     public void save(Tournament tournament)
