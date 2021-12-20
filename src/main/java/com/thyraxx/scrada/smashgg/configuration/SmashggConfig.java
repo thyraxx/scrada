43bba2cd3767d8339184f23282007ae7d692a7ec
X <taz3008@msn.com>
2021-12-20 22:58:38 +0100

    Changed api call delay to 60 seconds

--- src/main/java/com/thyraxx/scrada/smashgg/configuration/SmashggConfig.java
+++ src/main/java/com/thyraxx/scrada/smashgg/configuration/SmashggConfig.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 public class SmashggConfig {
 
     // Delay in milliseconds
-    public static final long smashggAPICallDelay = 90000;
+    public static final long smashggAPICallDelay = 60000;
     public static final String searchTournamentsAfterEpochTime = "{ \"lastDate\":" + (Instant.now().getEpochSecond() - 262928) +"}"; // 262928 aprox 3 days
 
 }
