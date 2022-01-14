5d1e524e2963ee8e5e750fdf961ae5b4b5c20ecd
X <taz3008@msn.com>
2022-01-14 19:02:51 +0100

    Removed unused import

--- src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
+++ src/main/java/com/thyraxx/scrada/smashgg/service/SmashggService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.*;
 import java.util.stream.Collectors;
 
