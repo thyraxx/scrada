8bd2de0c68bc3557bbcd815aefba8f8d8804bfd0
X <taz3008@msn.com>
2022-01-14 21:03:01 +0100

    Added configurable telegram chat id

--- src/main/java/com/thyraxx/scrada/customproperties/CustomProperties.java
+++ src/main/java/com/thyraxx/scrada/customproperties/CustomProperties.java
@@ -9,15 +9,24 @@ public class CustomProperties {
 
     private static final Logger logger = Logger.getLogger(CustomProperties.class);
     private static String smashggApiKey;
+    private static Long telegramChatId;
 
     public static String getSmashggApiKey()
     {
         return smashggApiKey;
     }
 
+    public static Long getTelegramChatId()
+    {
+        return telegramChatId;
+    }
+
     @Autowired
-    public CustomProperties(@Value("${smashggapikey}") String smashggApiKey)
+    public CustomProperties(
+            @Value("${smashggapikey}") String smashggApiKey,
+            @Value("${telegram_chat_id}") Long telegramChatId)
     {
         CustomProperties.smashggApiKey = smashggApiKey;
+        CustomProperties.telegramChatId = telegramChatId;
     }
 }
