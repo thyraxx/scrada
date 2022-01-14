3f5cc77e0267f585c6310290f17b658bf2148393
X <taz3008@msn.com>
2022-01-14 21:36:25 +0100

    Telegram bot is initialized with configurable bot api key

--- src/main/java/com/thyraxx/scrada/telegrambot/SmashggTelegramBot.java
+++ src/main/java/com/thyraxx/scrada/telegrambot/SmashggTelegramBot.java
@@ -5,6 +5,7 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import com.thyraxx.scrada.customproperties.CustomProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +18,7 @@ public class SmashggTelegramBot {
     private static final Logger logger = LoggerFactory.getLogger(SmashggTelegramBot.class);
 
     //TODO: Seperate token into config
-    private static final TelegramBot smashTelegramBot = new TelegramBot("");
+    private static final TelegramBot smashTelegramBot = new TelegramBot(CustomProperties.getTelegramBotKey());
 
     public static TelegramBot smashTelegramBotInstance() {
         return smashTelegramBot;
