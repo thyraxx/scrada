6439b020047be400a2a0fc89b60c4f83fd7f9c25
X <taz3008@msn.com>
2022-01-25 03:49:39 +0100

    Added API for retrieving all SmashTournaments in JSON

--- src/main/java/com/thyraxx/scrada/smashgg/controller/SmashggController.java
+++ src/main/java/com/thyraxx/scrada/smashgg/controller/SmashggController.java
@@ -1,13 +1,16 @@
 package com.thyraxx.scrada.smashgg.controller;
 
+import com.google.gson.Gson;
 import com.thyraxx.scrada.smashgg.service.SmashggService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/api/smashgg")
 public class SmashggController {
 
     private final SmashggService smashggService;
@@ -17,29 +20,30 @@ public class SmashggController {
         this.smashggService = smashggService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(Model model)
     {
         return "index";
     }
 
-    @GetMapping("/test")
-    public String test(Model model)
+//    @GetMapping("/test")
+//    public String test(Model model)
+//    {
+//        smashggService.saveNewTournamentEvents();
+//        return "index";
+//    }
+
+    @GetMapping("/getAllTournaments")
+    @ResponseBody
+    public String getAllTournaments()
     {
-        smashggService.saveNewTournamentEvents();
-        return "index";
+        return new Gson().toJson(smashggService.getAllTournamentsDTO());
     }
 
-    @GetMapping("/smashgg")
+    @GetMapping("/")
     public String smashTournaments(Model model) {
         model.addAttribute("tournaments", smashggService.getAllTournamentsDTO());
 
-        return "smashgg";
-    }
-
-    @PostMapping("/smashgg")
-    public String smashTournaments()
-    {
         return "smashgg";
     }
 }
