dedcc85879ab265fbde1a2639df10cb38cf183e4
X <taz3008@msn.com>
2022-01-26 05:10:05 +0100

    Added check for empty tournaments

--- src/main/java/com/thyraxx/scrada/smashgg/controller/SmashggController.java
+++ src/main/java/com/thyraxx/scrada/smashgg/controller/SmashggController.java
@@ -3,6 +3,7 @@ package com.thyraxx.scrada.smashgg.controller;
 import com.google.gson.Gson;
 import com.thyraxx.scrada.smashgg.model.Tournament;
 import com.thyraxx.scrada.smashgg.service.SmashggService;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,11 +34,18 @@ public class SmashggController {
     }
 
     @PostMapping("/saveTournament")
+    @ResponseBody
     public String saveTournament(@RequestBody Tournament tournament)
     {
+        // TODO: use httpstatus?
+        if(tournament.getEvents().isEmpty())
+        {
+            return "Tournament can't be added if there aren't any events.";
+        }
+
         // TODO: change saving to a boolean to know if it actually is saved?
         smashggService.save(tournament);
-        return "Saved!";
+        return "Created!";
     }
 
     @GetMapping("/")
