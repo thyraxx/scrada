b18c367737042335e2a0f4a7d416b071e1678538
X <taz3008@msn.com>
2022-02-22 16:42:12 +0100

    Changed getDateTime to clock

--- src/main/java/com/thyraxx/scrada/scheduler/Scheduler.java
+++ src/main/java/com/thyraxx/scrada/scheduler/Scheduler.java
@@ -1,6 +1,5 @@
 package com.thyraxx.scrada.scheduler;
 
-import com.thyraxx.scrada.customproperties.CustomProperties;
 import com.thyraxx.scrada.smashgg.configuration.SmashggConfig;
 import com.thyraxx.scrada.smashgg.service.SmashggService;
 import org.slf4j.Logger;
@@ -9,35 +8,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Clock;
+
 @Component
 public class Scheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(Scheduler.class);
 
     private final SmashggService smashggService;
+    private final Clock clock;
 
     @Autowired
-    public Scheduler(SmashggService smashggService) {
+    public Scheduler(SmashggService smashggService, Clock clock) {
         this.smashggService = smashggService;
+        this.clock = clock;
     }
 
     @Scheduled(fixedDelay = SmashggConfig.SMASHGG_API_CALL_DELAY) // 60 seconds
     public void retrieveSmashTournamentData()
     {
-        logger.debug("Retrieving data on: {}-> using method 'retrieveSmashTournamentData'", CustomProperties.getDateTime());
+        logger.debug("Retrieving data on: {} -> using method 'retrieveSmashTournamentData'", clock.instant());
         smashggService.saveNewTournamentEvents();
 
 //        logger.debug("Updating data on: " + dateFormat  + " -> using method 'retrieveSmashTournamentData'");
 //        smashggService.updateExistingTournaments();
 
-        logger.debug("Sending notification on: {} -> using method 'retrieveSmashTournamentData'", CustomProperties.getDateTime());
+        logger.debug("Sending notification on: {} -> using method 'retrieveSmashTournamentData'", clock.instant());
         smashggService.sendNotification();
     }
 
     @Scheduled(fixedDelay = 43200000) // 12 hours
     public void deleteFinishedTournaments()
     {
-       logger.debug("Deleting expired/finished tournaments data on: {} -> using method 'deleteFinishedTournaments'", CustomProperties.getDateTime());
+       logger.debug("Deleting expired/finished tournaments data on: {} -> using method 'deleteFinishedTournaments'", clock.instant());
         smashggService.deleteFinishedTournaments();
     }
 }
