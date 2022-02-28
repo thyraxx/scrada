3d33c8ad69c4c10e724522cf80b18619e7df948b
X <taz3008@msn.com>
2022-02-28 20:44:31 +0100

    Added telegram entity

---
+++ src/main/java/com/thyraxx/scrada/model/Telegram.java
@@ -0,0 +1,44 @@
+package com.thyraxx.scrada.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Telegram {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String telegramBotKey;
+
+    @ElementCollection
+    private List<Long> chatIdList;
+
+    @ManyToOne
+    private UserModel userModel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTelegramBotKey() {
+        return telegramBotKey;
+    }
+
+    public void setTelegramBotKey(String telegramBotKey) {
+        this.telegramBotKey = telegramBotKey;
+    }
+
+    public UserModel getUser() {
+        return userModel;
+    }
+
+    public void setUser(UserModel userModel) {
+        this.userModel = userModel;
+    }
+}
